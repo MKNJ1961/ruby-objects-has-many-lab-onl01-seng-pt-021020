ac932746bea4da8646c81631299cf131cce9c274
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 00:49:19 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+Learn more or give us feedback
+require_relative 'post'
+
+class Author
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+  end
+
+  def add_post_by_title(title)
+    p = Post.new(title)
+    p.author = self
+    @posts << p
+  end
+
+  def self.post_count
+    Post.all.length
+  end
+end
