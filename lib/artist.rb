04180677bd3af233c9efca3b19fc184385769b89
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 02:10:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,37 +32,37 @@ end
 
 
 # require_relative 'song'
-# 
+#
 # class Artist
 #   attr_accessor :name, :songs
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @songs = []
 #   end
-# 
+#
 #   def self.song_count
 #    @@song_count
 #  end
-# 
+#
 #  # def songs
 #  #   @songs
 #  # end
-# 
-# 
+#
+#
 #   def add_song(song)
 #     song.artist = self
 #     self.songs << song
 #   end
-# 
+#
 #   def add_song_by_name(name)
 #     s = Song.new(name)
 #     s.artist = self
 #     self.songs << s
 #   end
-# 
+#
 #   def self.song_count
 #     Song.all.length
 #   end
-# 
+#
 # end
